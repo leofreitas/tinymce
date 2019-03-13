2511192140b67dab99476cdc46ebffccc88cdd15
leonardofreitas <wwwlfticombr@gmail.com>
2019-03-12 22:14:44 -0300

    Correção de erros de compilação

--- src/plugins/infografico/main/ts/ui/ContentTab.ts
+++ src/plugins/infografico/main/ts/ui/ContentTab.ts
@@ -2,11 +2,11 @@ import Settings from '../api/Settings';
 import { document, HTMLInputElement } from '@ephox/dom-globals';
 
 const registerText = function (editor) {
-   const cont = editor.getBody();
-  const ni = Settings.getNumberItems(editor)+2;
+  const cont = editor.getBody();
+  const ni = Settings.getNumberItems(editor) + 2;
   let addHtml = '';
   if (ni > 0) {
-    for (const i = 2; i < ni; i++) {
+    for (let i = 2; i < ni; i++) {
       const bl = document.getElementById('bloco' + i) as HTMLInputElement;
       if (bl.value !== '') {
         const linkElm = editor.dom.createHTML('div', {
@@ -17,30 +17,29 @@ const registerText = function (editor) {
         addHtml += linkElm;
       }
     }
-    const divMaster = editor.dom.createHTML('div', { id: 'divmaster', name: 'divmaster', contenteditable: 'false' }, addHtml);
+    const divInfografico = editor.dom.createHTML('div', { id: 'info-main', name: 'info-main', contenteditable: 'false' }, addHtml);
     if (addHtml !== '') {
-       cont.innerHTML = divMaster;
+       cont.innerHTML = divInfografico;
     }
   }
 };
 
 const getContentItems = function (editor) {
     const generalContentItems = [];
-    const ni = Settings.getNumberItems(editor)+2;
+    const ni = Settings.getNumberItems(editor) + 2;
 
     if (ni > 0) {
-        let blocoValue = '';
-        let indice = 0;
-        for (const i = 2; i < ni; i++) {
-            indice = i - 1;
-            if(editor.dom.get('contentBlock'+i)){
-              blocoValue = editor.dom.get('contentBlock'+i).innerHTML;
-            }
-            else{
-              blocoValue = '';
-            }
-            generalContentItems.push( { name: 'bloco' + i , id: 'bloco' + i, type: 'textbox', label: 'Bloco ' + indice, value:  blocoValue } );
+      let blocoValue = '';
+      let indice = 0;
+      for (let i = 2; i < ni; i++) {
+        indice = i - 1;
+        if (editor.dom.get('contentBlock' + i)) {
+           blocoValue = editor.dom.get('contentBlock' + i).innerHTML;
+        } else {
+          blocoValue = '';
         }
+        generalContentItems.push( { name: 'bloco' + i , id: 'bloco' + i, type: 'textbox', label: 'Bloco ' + indice, value:  blocoValue } );
+      }
     }
 
     return generalContentItems;
