50746e01a95140cde6825427e2c31302e7b555dd
leonardofreitas <leonardofreitas.lfti@gmail.com>
2019-05-22 15:10:45 -0300

    Correção do atributo de edição do infográfico 01

--- src/plugins/infografico/demo/ts/demo/Demo.ts
+++ src/plugins/infografico/demo/ts/demo/Demo.ts
@@ -12,7 +12,7 @@ declare let tinymce: any;
 
 tinymce.init({
   selector: 'textarea.tinymce',
-  extended_valid_elements: 'div[id|style|shapeitems|numberitems|typemarcador|backgroundcolorbox|bordercolorbox|textcolorbox]',
+  extended_valid_elements: 'div[id|style|shapeitems|numberitems|typemarcador|backgroundcolorbox|bordercolorbox|textcolorbox|contenteditable]',
   theme: 'modern',
   skin_url: '../../../../../js/tinymce/skins/lightgray',
   plugins: 'infografico code',
